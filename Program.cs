ff00ccf7ebbed13a04fe351ebef2e80210157d56
Misha Bespalov <mishabespalov@yandex.ru>
2018-05-28 22:07:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace bot
+{
+    class Program
+    {
+		private DiscordSocketClient client;
+
+        static void Main(string[] args)
+        {
+			new Program().StartAsync().GetAwaiter().GetResult();
+        }
+
+		private async Task StartAsync()
+		{
+			client = new DiscordSocketClient();
+			await client.LoginAsync(TokenType.Bot, "NDUwNjUzNTMzNDk1MDk5Mzkz.De2cPA.26XcebFedCvxlXmczqAggo9_iFQ");
+			await client.StartAsync();
+			Console.WriteLine("1");
+			await Task.Delay(-1);
+			Console.WriteLine("2");
+		}
+    }
+}
